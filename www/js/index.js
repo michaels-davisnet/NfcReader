5434560c93d1a5ea6cea0591fe44a53a4219b0e6
michaels-davisnet <michaels@davisnet.com>
2014-09-22 17:43:16 -0700

    Android App can read NFC payload containing BLE MAC addr and auto connect to BLE Arduino

--- www/js/index.js
+++ www/js/index.js
@@ -5,39 +5,56 @@ var app = {
 	bindEvents : function () {
 		document.addEventListener('deviceready', this.onDeviceReady, false);
 	},
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	MACaddress : "FA:22:C8:BB:93:0B",
 	onDeviceReady : function () {
 		// Params: event handler function, success callback, failure callback.
 		nfc.addMimeTypeListener("text/pan", app.onNfc, initSuccess, initError);
+		// TODO: NEED TO HANDLE OTHER NFC TYPES!
 		function initSuccess(status) {
-			app.display("Tap a tag to read its id number.");
+			app.display("Place your phone on MAIA device");
 		}
 		function initError(error) {
-			app.display("NFC reader failed to initialize " + JSON.stringify(error));
+			app.display("NFC reader failed to initialize. Is NFC enabled? " + JSON.stringify(error));
 		}
-
 	},
-
 	onNfc : function (nfcEvent) {
 		app.clear();
 		var tag = nfcEvent.tag;
-		app.display("Tag ID: " + nfc.bytesToHexString(tag.id));
+		// app.display("Tag ID: " + nfc.bytesToHexString(tag.id));
+		app.display("MAIA Device Detected");
 		app.showTag(nfcEvent.tag); // display the tag details
 	},
-
 	showTag : function (tag) {
 		// if there is an NDEF message on the tag, display it.
 		var message = tag.ndefMessage;
 		if (message !== null) {
 			var record = message[0];
-			app.display("TNF: " + record.tnf);
-			app.display("Type: " + nfc.bytesToString(record.type));
-			app.display("Payload: " + nfc.bytesToString(record.payload));
-		}
-		else {
-			app.display("No message");
+			// app.display("TNF: " + record.tnf);
+			// app.display("Type: " + nfc.bytesToString(record.type));
+			app.MACaddress = nfc.bytesToString(record.payload);
+			app.display("MAC: " + app.MACaddress);
+			app.bleConnect();
+		} else {
+			app.display("No NFC message");
 		}
 	},
-
+	bleConnect : function () {
+		app.display("Scanning for BLE connection");
+		ble.scan([], 5, scanned, failure);
+		function scanned(device) {
+			ble.connect(app.MACaddress, function () {
+				app.display("Connected to: " + device.name);
+				// TODO: Read BLE data from here
+			}, function () {
+				app.display("Failed to Connect");
+			});
+		}
+		function failure() {
+			app.display("Bluetooth Low Energy Error");
+		}
+	},
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	display : function (message) {
 		var label = document.createTextNode(message);
 		var lineBreak = document.createElement("br");
